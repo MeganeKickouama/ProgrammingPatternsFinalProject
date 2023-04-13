77ea7c832bb5c9fc5fcf4b0e4ee9c7a33ed9ce15
hqara <98411846+hqara@users.noreply.github.com>
2023-04-13 15:48:58 -0400

    Update Account.java
    For starters, attributes, and methods to work with

--- librarymanagement/Account.java
+++ librarymanagement/Account.java
@@ -10,4 +10,26 @@ package librarymanagement;
  */
 public class Account {
     
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+    private int id; 
+    //some fields may be unnecessary, feel free to make it redundant
+   
+
+    public Account(String username, String password, String email, String phone, int id) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.id = id;
+    }
+
+     public Book searchBook(String bookTitle) {
+        // Code to search for a book in the library
+    }
+    
+    //Will add getters and setters for all fields later
+
 }
