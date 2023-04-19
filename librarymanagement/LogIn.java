58a0d584e923b04c4b3482cc38a09c45b5fd093a
sadafzakria <102993150+sadafzakria@users.noreply.github.com>
2023-04-19 15:38:33 -0400

    Update Login(factory

--- librarymanagement/LogIn.java
+++ librarymanagement/LogIn.java
@@ -8,6 +8,30 @@ package librarymanagement;
  *
  * @author szakr
  */
-public class LogIn {
-    
+public abstract class LogIn {
+    abstract void AccountLoggedIn();
 }
+class LogInFactory{
+    static public Account createAccountType(String AccountId){
+        if(AccountId.startsWith("1")){
+            //System.out.println("New Staff");
+            Account s = new Staff(AccountId, "Abc123");
+            Library.staffDatabase.put(Integer.parseInt(AccountId), s);
+            return s;
+        }
+        else if(AccountId.startsWith("2")){
+            //System.out.println("New Supplier");
+            Account sup = new Supplier(AccountId, "Def123");
+            Library.supplierDatabase.put(Integer.parseInt(AccountId), sup);
+            return sup;
+        }
+        else if(AccountId.startsWith("3")){
+            //System.out.println("New User");
+            Account user = new User(AccountId, "Ghi123");
+            Library.userDatabase.put(Integer.parseInt(AccountId), user);
+            return user;
+        }
+        return null;
+        
+    } 
+} 
