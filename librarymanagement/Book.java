5d045105ed9025e02d39c970b5d9e25eb85c4d4c
Megane <98486075+MeganeKickouama@users.noreply.github.com>
2023-04-20 10:29:28 -0400

    Book update with counter & constructor.
    Modify counter later.

--- librarymanagement/Book.java
+++ librarymanagement/Book.java
@@ -4,14 +4,33 @@
  */
 package librarymanagement;
 
+import java.io.Serializable;
+
 /**
  *
  * @author szakr
  */
-public class Book {
+public class Book implements Serializable {
     private String title;
     private String author;
     private int bookID;
     private int price;
     private static int count = 0;
+
+    public Book(String title, String author, int price)
+    {
+        this.bookID = nextID();
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public static int nextID()
+    {
+        // to modify later, I am using this for a test
+        count ++;
+        return count;
+    }
+
+
 }
